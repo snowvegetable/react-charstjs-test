9e4597c82227acd80cc6248e5f15deca5b2547c2
snowvegetable <a47462543@gmail.com>
2022-05-11 15:07:03 +0800

    測試chartjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import BarChart from "./components/BarChart";
+import "./App.css";
+import { UserData } from "./Data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userData, setUserData] = useState({
+        // labels: UserData.map((data) => data.year),
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+            {
+                label: "Users Gained",
+                // data: UserData.map((data) => data.userGain),
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99,  132, 0.2)',
+                    'rgba(54,  162, 235, 0.2)',
+                    'rgba(255, 206, 86,  0.2)',
+                    'rgba(75,  192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99,  132, 1)',
+                    'rgba(54,  162, 235, 1)',
+                    'rgba(255, 206, 86,  1)',
+                    'rgba(75,  192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                ],
+                borderWidth:3,
+            },
+        ],
+    });
+
+
+    return (
+        <div className="App">
+            <div style={{ width: 700}}>
+                <BarChart chartData={userData} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
